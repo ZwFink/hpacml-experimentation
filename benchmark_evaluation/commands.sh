1223bb2d9280cd2dd82a64d49fa970a02734e9f7
Zane Fink <finkzane@gmail.com>
2024-06-26 10:21:25 -0500

    add local arg

--- benchmark_evaluation/commands.sh
+++ benchmark_evaluation/commands.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-python3 driver.py --config config.yaml --benchmark bonds --output 2024-03-17_bonds.csv
-python3 driver.py --config config.yaml --benchmark binomialoptions --output 2024-03-15_binomial_options.csv
-python3 driver.py --config config.yaml --benchmark minibude --output 2024-03-28_minibude_output.csv
-python3 driver.py --config config.yaml --benchmark miniweather --output 2024-03-19_miniweather.csv
-python3 driver.py --config config.yaml --benchmark particlefilter --output 2024-03-14_particlefilter.csv
-
+python3 driver.py --config config.yaml --benchmark bonds --output bonds.csv --local
+python3 driver.py --config config.yaml --benchmark binomialoptions --output binomial_options.csv --local
+python3 driver.py --config config.yaml --benchmark minibude --output minibude_output.csv --local
+python3 driver.py --config config.yaml --benchmark miniweather --output miniweather.csv --local
+python3 driver.py --config config.yaml --benchmark particlefilter --output particlefilter.csv --local
